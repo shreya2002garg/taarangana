7e4f540bcb404acfc93f584367ff8ee286442398
Shreya Garg <111244172+shreya2002garg@users.noreply.github.com>
2023-09-09 13:53:53 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,37 @@
+// const navbar = document.getElementById("navBar");
+// const showNavbarButton = document.getElementById("showNavBarIcon");
+// const body = document.getElementById("main-sec");
+
+// showNavbarButton.addEventListener("click", function () {
+//     if (navbar.style.display === "none") {
+//         navbar.style.display = "inline";
+//     } else {
+//         navbar.style.display = "none";
+//         body.style.marginLeft = "0px";
+//     }
+// });
+
+
+// const home = document.getElementsByClassName("about")
+// const gallery = document.getElementsByName("gallery")
 const navbar = document.getElementById("navBar");
 const showNavbarButton = document.getElementById("showNavBarIcon");
 const body = document.getElementById("main-sec");
+const galleryImages = document.getElementsByClassName("img");
+const mainSecGallery = document.getElementById("gallery-sec")
 
 showNavbarButton.addEventListener("click", function () {
-    if (navbar.style.display === "none") {
-        navbar.style.display = "inline";
+    if (navbar.style.display === "none" || navbar.style.display === "") {
+        navbar.style.display = "block"; // Change to "block" to display the navbar
+        for (let i = 0; i < galleryImages.length; i++) {
+            galleryImages[i].style.width = "24vw";
+            mainSecGallery.style.marginLeft = "270px"
+        }
     } else {
         navbar.style.display = "none";
-        body.style.marginLeft = "0px";
+        for (let i = 0; i < galleryImages.length; i++) {
+            galleryImages[i].style.width = ""; // Reset the width
+            galleryImages[i].style.marginLeft = ""; // Reset the margin
+        }
     }
 });
-
-
-const home = document.getElementsByClassName("about")
-const gallery = document.getElementsByName("gallery")
